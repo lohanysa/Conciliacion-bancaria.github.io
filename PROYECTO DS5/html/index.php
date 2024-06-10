2cce140557d6314a91dde2857583a489d51b980d
Jesus Claros <164571471+jesusclaros@users.noreply.github.com>
2024-06-10 13:17:02 -0500

    guarde script

--- PROYECTO DS5/html/index.php
+++ PROYECTO DS5/html/index.php
@@ -604,11 +604,6 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 });
 
-
-
-
-
-
 //verificar la fecha de anulacion sea menor a cuando se creo
 var fechaAnulacion = document.getElementById('fechaAnulacion'); // fecha de creacion
 fechaAnulacion.addEventListener('blur', function(e) {
@@ -640,6 +635,75 @@ fechaCirculacionzin.addEventListener('blur', function(e){
         document.getElementById('Anulacionzion').disabled = false;
     }
 })
+</script>
+
+<script>
+   //scrip para enviar el archivo 
+   document.getElementById('enviar').addEventListener('click', function(e){
+    e.preventDefault();
+    document.getElementById('mensaje').value = 'El archivo esta cargando, por favor espere...'
+    var archivoData = new FormData();
+    archivoData.append('archivo', document.getElementById('archivo').files[0]);
+    fetch('../php/asistencia.php', {
+        method: 'POST',
+        body: archivoData
+    })
+    .then(respuesta => respuesta.json())
+    .then(respuesta => {
+        document.getElementById('mensaje').value = respuesta
+        
+        
+    });
+});
+
+//escrip para validar las fechas
+
+document.getElementById("Fecha_final").addEventListener("blur", function(e){
+    e.preventDefault();
+    final = document.getElementById("Fecha_final").value || 0;
+    inicio = document.getElementById("Fecha_inicial").value || 0;
+
+    if(final<inicio){
+        document.getElementById('mensaje').value = "La fecha final no puede ser menor que la fecha inicial";
+        document.getElementById("Fecha_final").value = "";
+        document.getElementById("Fecha_inicial").value= "";
+        document.getElementById("buscar_marcacion").disabled = true
+        document.getElementById("buscar_marcacion").style.backgroundColor = "gray";
+        document.getElementById("buscar_marcacion").style.color = "white";
+    }else{
+        document.getElementById("buscar_marcacion").disabled = false
+        document.getElementById("buscar_marcacion").style.backgroundColor = "";
+        document.getElementById("buscar_marcacion").style.color = "";
+        document.getElementById('mensaje').value = "";
+    }
+})
+
+//este es el scrip del boton buscar 
+document.getElementById("buscar_marcacion").addEventListener("click", function(e) {
+    e.preventDefault();
+    const fechas = new FormData();
+    fechas.append("inicio", document.getElementById("Fecha_inicial").value);
+    fechas.append("final", document.getElementById("Fecha_final").value);
+    fechas.append("codigo", document.getElementById("nombre_asistencias").value);
+
+    
+
+    fetch('../php/pdf.php', {
+        method: "POST",
+        body: fechas
+    })
+    .then(response => response.blob())
+    .then(blob => {
+        const url = URL.createObjectURL(blob);
+        window.open(url, '_blank');
+        URL.revokeObjectURL(url); // Limpieza de la URL del objeto
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        document.getElementById('mensaje').value = 'Error al generar el reporte.';
+    });
+});
+
 </script>
 
 </body>
